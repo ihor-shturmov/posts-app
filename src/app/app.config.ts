9fc985720326677009ec0dae3fdd203f02bbdad8
Igor Shturmov <shturmik@gmail.com>
2025-05-01 18:15:03 +0200

    Added some basic styling.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,11 +7,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations()
   ]
 };
--- src/app/components/post-square/post-square.component.ts
+++ src/app/components/post-square/post-square.component.ts
@@ -1,14 +1,30 @@
 import { Component, computed, input, InputSignal, output, OutputEmitterRef } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Post } from '../../core/models/post.model';
 
 @Component({
   selector: 'app-post-square',
   imports: [],
   template: `
-    <div class="square-content" (click)="squareClicked.emit(post().id)">
+    <div [@fadeInOut]="displayValue()"
+         class="square-content"
+         [class.active]="isActive()"
+         (click)="squareClicked.emit(post().id)">
       {{ displayValue() }}
     </div>
   `,
+  animations: [
+    trigger('fadeInOut', [
+      transition(':increment', [
+        style({ opacity: 0 }),
+        animate('150ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':decrement', [
+        style({ opacity: 0 }),
+        animate('150ms ease-in', style({ opacity: 1 }))
+      ]),
+    ])
+  ],
   styleUrl: './post-square.component.scss'
 })
 export class PostSquareComponent {
